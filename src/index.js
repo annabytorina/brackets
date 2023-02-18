5ac4de6e77590c6240b0a778904d04d1f9d257ed
annabytorina <hannabutorina@gmail.com>
2023-02-18 19:52:04 +0300

    feat: add func without |

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const OPEN_BRACKETS = ['(','{','[','|'];
+const BRACKETS_PAIR = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['|']: '|',
+};
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for(let i=0; i<str.length;i++) {
+      let currBracket = str[i];
+
+      if (OPEN_BRACKETS.includes(currBracket)) {
+          stack.push(currBracket);
+      } else {
+          if (stack.length === 0) {
+              return false;
+          }
+          let top = stack[stack.length -1];
+
+          if (BRACKETS_PAIR[currBracket] === top) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        }
+  }
+
+  return stack.length === 0;
 }
